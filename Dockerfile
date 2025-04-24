c673e1f9d674ca89c8d63c65ac42f3ee45c9a35d
shahabas <mohdshahabasm@gmail.com>
2025-04-24 11:14:35 +0300

    updated without jenkins file

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+From python:3.8-slim-buster
+
+RUN apt update -y && apt install awscli -y
+
+WORKDIR  /app
+
+COPY . /app/
+
+RUN pip install -r requirements.txt
+
+# EXPOSE 8080
+
+CMD ["python3","app.py"]
---
+++ main.py
@@ -0,0 +1,63 @@
+from ml_project import logger
+from ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ml_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from ml_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from ml_project.pipeline.stage_04_model_training import ModelTrainerTrainingPipeline
+from ml_project.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
+logger.info("welcome to the project")
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Training stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_training = ModelTrainerTrainingPipeline()
+   model_training.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
